51f7a924eed29c01ab1f4dab4d7d6637f0b5534e
KacperGora <kacpergoora@gmail.com>
2024-12-20 10:09:39 +0100

    feat: add file upload functionality with multer

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var cors = require('cors');
+var express = require('express')
+var cors = require('cors')
+var multer = require('multer')
 require('dotenv').config()
 
-var app = express();
+var app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
+app.post('/api/fileanalyse', function (req, res) {
+ 
+  const storage = multer.memoryStorage()
+  const upload = multer({ storage: storage })
 
+  upload.single('upfile')(req, res, function (err) {
+    if (err) {
+      res.json({ error: err.message })
+    } else {
+      res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size })
+    }
+  }
+  )
+  // res.json({type: req.file.mimetype, size: req.file.size })
+})
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
